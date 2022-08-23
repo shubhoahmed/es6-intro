213c4429615af5dd5fa366da4d98276a9a2b0f57
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-23 23:03:30 +0600

    add new friend by js dynamic

--- js/dom.js
+++ js/dom.js
@@ -5,3 +5,18 @@ document.getElementById('apply-bg').addEventListener('click', function () {
     }
 
 })
+document.getElementById('do-center').addEventListener('click', function () {
+    const firstFriend = document.getElementById('friend-one');
+    firstFriend.style.textAlign = 'center';
+})
+
+document.getElementById('add-friend').addEventListener('click', function () {
+    const friendContainer = document.getElementById('friend-container');
+    const friend = document.createElement('div')
+    friend.classList.add('friend');
+    friend.innerHTML = `  
+                 <h3 class="friend-name">New Friend</h3>
+                 <p>Lorem ipsum dolor sit amet.</p>
+             `
+    friendContainer.appendChild(friend);
+})
