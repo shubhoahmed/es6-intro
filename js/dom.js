dd135f35081bd315316e1970b707538129349102
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-23 22:42:56 +0600

    getElementByClasssName

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+document.getElementById('apply-bg').addEventListener('click', function () {
+    const friends = document.getElementsByClassName('friend');
+    for (const friend of friends) {
+        friend.style.backgroundColor = 'red';
+    }
+
+})
